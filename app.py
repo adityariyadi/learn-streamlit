cfd4616ce10d569c396e093b3a7d1ce547d9b6fb
Aditya Riyadi <ph@powerhouse.network>
2023-10-07 14:17:15 +0700

    additional files and file changes

--- app.py
+++ app.py
@@ -1,64 +1,22 @@
-import streamlit as st;
-import streamlit.components as stc
-import base64
-import time
-import pandas as pd
-
-
-timestr = time.strftime("%Y%m%d-%H%M%S")
-
-def text_downloader(raw_text):
-    b64 = base64.b64encode(raw_text.encode()).decode()
-    new_filename = "new_text_file_{}_.txt".format(timestr)
-    st.markdown("#### Donwload File ###")
-    href = f'<a href="data:file/txt;base64,{b64}" download="{new_filename}">Click Here!</a>'
-    st.markdown(href, unsafe_allow_html=True)
-
-def csv_downloader(data):
-    csvfile = data.to_csv()
-    b64 = base64.b64encode(csvfile.encode()).decode()
-    new_filename = "new_text_file_{}_.csv".format(timestr)
-    st.markdown("#### Donwload File ###")
-    href = f'<a href="data:file/csv;base64,{b64}" download="{new_filename}">Click Here!</a>'
-    st.markdown(href, unsafe_allow_html=True)
-
-# Class
-class FileDownLoader(object):
-    def __init__(self, data, filename='download_file', file_ext='txt'):
-        super(FileDownLoader, self).__init__()
-        self.data = data
-        self.filename = filename
-        self.file_ext = file_ext
-
-    def download(self):
-        b64 = base64.b64encode(self.data.encode()).decode()
-        new_filename = "{}_{}_.{}".format(self.filename, timestr, self.file_ext)
-        st.markdown("#### Donwload File ###")
-        href = f'<a href="data:file/{self.file_ext};base64,{b64}" download="{new_filename}">Click Here!</a>'
-        st.markdown(href, unsafe_allow_html=True)
-
+import streamlit as st
 
 def main():
-    menu = ["Home", "CSV", "About"]
-    choice = st.sidebar.selectbox("Menu", menu)
+    st.title("Streamlit App")
+    st.subheader("Form Reset")
 
-    if choice=="Home":
-        st.subheader("Home")
+    #Docs
+    st.help(st.form)
+
+    with st.form(key='mForm', clear_on_submit=True):
+        firstname = st.text_input("First name")
+        lastname = st.text_input("Last name")
         message = st.text_area("Message")
-        if st.button("Save"):
-            st.write(message)
-            # text_downloader(message)
-            download = FileDownLoader(message).download()
+        submit_button = st.form_submit_button("Submit")
 
-    elif choice=="CSV":
-        df = pd.read_csv("iris.csv")
-        st.dataframe(df)
-        # csv_downloader(df)
-        download = FileDownLoader(df.to_csv(), file_ext='csv').download()
-    else:
-        st.subheader("About")
+    if submit_button:
+        st.info("Results")
+        result = firstname + lastname + '@gmail.com'
+        st.write(result)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
---
+++ app_forms.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import pandas as pd
+
+def main():
+    st.title("Streamlit Forms & Salary Calculator")
+    menu = ["Home", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice=="Home":
+        st.subheader("Form Tutorial")
+
+        with st.form(key='salaryform'):
+            col1,col2,col3= st.columns([3,2,1])
+            with col1:
+                amount = st.number_input("Hourly Rate in $",1,120)
+            with col2:
+                hour_per_week = st.number_input("Hours per week")
+            with col3:
+                st.text("Salary")
+                submit_salary = st.form_submit_button(label='Calculate')
+
+
+        if submit_salary:
+            with st.expander("Results"):
+                daily = [amount * 8]
+                weekly = [amount * hour_per_week]
+                df = pd.DataFrame({'hourly': amount, 'daily': daily, 'weekly': weekly})
+                st.dataframe(df.T) 
+
+
+        with st.form(key='form1'):
+            firstname = st.text_input("First name")
+            lastname = st.text_input("Last name")
+            dob = st.date_input("Date of Birth")
+            submit_button = st.form_submit_button(label='Signup')
+
+    
+        # Method 1
+        if submit_button:
+            st.success("Hello {} you have created an account".format(firstname))
+
+        # Method 2
+        form2 = st.form(key='form2')
+        username = form2.text_input("Username")
+        jobtype = form2.selectbox("Job", ["Dev", "Data Scientist", "Doctor"])
+        submit_button2 = form2.form_submit_button("Login")
+
+        if submit_button2:
+            st.write(username.upper())
+
+    else:
+        st.subheader("header")
+
+
+if __name__ == '__main__':
+    main()
